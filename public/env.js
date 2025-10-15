6498d2ac4d0c9ebeb228f5d4dd28cff3e11beef4
Rakibul Hasan Shaon <50649996+Lil-boss@users.noreply.github.com>
2025-10-16 01:14:15 +0600

    Update VITE_APP_API endpoint URL

--- public/env.js
+++ public/env.js
@@ -1,5 +1,5 @@
 // window.env = { VITE_APP_API: "https://api.saiyed.vara.ac", VITE_APP_BASE: "/" };
 window.env = {
-  VITE_APP_API: "https://backend.anondini.com/",
+  VITE_APP_API: "https://backend-app-egcvc0a7dxfjhjff.malaysiawest-01.azurewebsites.net/",
   VITE_APP_BASE: "/",
 };
