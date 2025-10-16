976f73479003b31d8faf01356df1e16ebcaea34a
Rakibul Hasan Shaon <50649996+Lil-boss@users.noreply.github.com>
2025-10-16 22:01:28 +0600

    Update env.js

--- public/env.js
+++ public/env.js
@@ -1,5 +1,5 @@
 // window.env = { VITE_APP_API: "https://api.saiyed.vara.ac", VITE_APP_BASE: "/" };
 window.env = {
-  VITE_APP_API: "https://38c5f266aa3e.ngrok-free.app/",
+  VITE_APP_API: "https://20-41-122-194.sslip.io",
   VITE_APP_BASE: "/",
 };
