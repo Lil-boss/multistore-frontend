cce1c4602f2a9d1d5223fe6eee308e0d8b96cc90
Rakibul Hasan Shaon <50649996+Lil-boss@users.noreply.github.com>
2025-10-16 19:59:52 +0600

    Update env.js

--- public/env.js
+++ public/env.js
@@ -1,5 +1,5 @@
 // window.env = { VITE_APP_API: "https://api.saiyed.vara.ac", VITE_APP_BASE: "/" };
 window.env = {
-  VITE_APP_API: "https://backend-app-egcvc0a7dxfjhjff.malaysiawest-01.azurewebsites.net/",
+  VITE_APP_API: "http://20.41.122.194/",
   VITE_APP_BASE: "/",
 };
