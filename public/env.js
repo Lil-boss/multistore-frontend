fa04a1230fff1b5c9b0cb7b1a1d8a73880e32f11
Rakibul Hasan Shaon <50649996+Lil-boss@users.noreply.github.com>
2026-05-01 23:17:47 +0600

    Update VITE_APP_API URL in env.js

--- public/env.js
+++ public/env.js
@@ -1,5 +1,5 @@
 // window.env = { VITE_APP_API: "https://api.saiyed.vara.ac", VITE_APP_BASE: "/" };
 window.env = {
-  VITE_APP_API: "multistore-backend-production.up.railway.app",
+  VITE_APP_API: "https://multistore-backend-production.up.railway.app",
   VITE_APP_BASE: "/",
 };
